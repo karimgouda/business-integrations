7d5a27cf6299ced1548f5ab9ed57e6e196bb9865
Karim_Gouda <105445250+karimgouda@users.noreply.github.com>
2025-07-03 14:00:45 +0300

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>karim gouda</title>
+        <title>karim</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
