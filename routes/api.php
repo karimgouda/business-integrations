b8c7c325ad615822affad500736c7a3b921cdb4e
karimgouda <dev.karimgouda@gmail.com>
2025-08-07 16:30:39 +0300

    dd

--- routes/api.php
+++ routes/api.php
@@ -5,5 +5,5 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-Route::post('/webhook', [MessengerController::class, 'handleWebhook']);
+Route::get('/webhook', [MessengerController::class, 'handleWebhook']);
 //Route::get('/webhook', [MessengerController::class, 'verifyWebhook']);
